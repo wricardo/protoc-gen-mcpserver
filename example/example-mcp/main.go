282f6025ffcbef51d5e560e8736aa35cb319ffc4
Wallace Ricardo <wallace.crd@gmail.com>
2025-04-25 11:42:36 -0400

    support other types

--- example/example-mcp/main.go
+++ example/example-mcp/main.go
@@ -10,26 +10,68 @@ import (
 type GreetServer struct {
 }
 
-func (s *GreetServer) Tool1(ctx context.Context, req Tool1Request) (*Tool1Response, error) {
+// CalculateSum implements example.ExampleServiceMcpServer.
+func (s *GreetServer) CalculateSum(ctx context.Context, req *CalculateSumRequest) (*CalculateSumResponse, error) {
+	return &CalculateSumResponse{
+		Sum:     req.Number1 + req.Number2,
+		Product: float64(req.Number1) * float64(req.Number2) * float64(req.Factor),
+	}, nil
+}
+
+func (s *GreetServer) CheckStatus(ctx context.Context, req *CheckStatusRequest) (*CheckStatusResponse, error) {
+	return &CheckStatusResponse{
+		Success: true,
+		Message: "Status is active",
+	}, nil
+}
+
+func (s *GreetServer) ComplexOperation(ctx context.Context, req *ComplexOperationRequest) (*ComplexOperationResponse, error) {
+	return &ComplexOperationResponse{
+		Success:     true,
+		OperationId: "12345",
+		StatusCode:  200,
+		Results:     []string{"Result1", "Result2"},
+		Average:     10.5,
+	}, nil
+}
+
+func (s *GreetServer) GreetPerson(ctx context.Context, req *GreetPersonRequest) (*GreetPersonResponse, error) {
+	return &GreetPersonResponse{
+		Greeting: "Hello, " + req.FirstName + " " + req.LastName,
+	}, nil
+}
+
+// ProcessNames implements example.ExampleServiceMcpServer.
+func (s *GreetServer) ProcessNames(ctx context.Context, req *ProcessNamesRequest) (*ProcessNamesResponse, error) {
+	return &ProcessNamesResponse{
+		ProcessedNames:  []string{"Processed1", "Processed2"},
+		ProcessedCounts: []int32{1, 2},
+		Summary:         "Processed 2 names",
+	}, nil
+}
+
+func (s *GreetServer) Tool1(ctx context.Context, req *Tool1Request) (*Tool1Response, error) {
 	return &Tool1Response{
 		Fullname: "Hello, " + req.Firstname + " " + req.Lastname,
 	}, nil
 }
 
-func (s *GreetServer) Tool2(ctx context.Context, req Tool2Request) (*Tool2Response, error) {
+func (s *GreetServer) Tool2(ctx context.Context, req *Tool2Request) (*Tool2Response, error) {
 	return &Tool2Response{
 		Result: "Hello, " + req.Name,
 	}, nil
 }
 
-func (s *GreetServer) Tool3(ctx context.Context, req Tool3Request) (*Tool3Response, error) {
+func (s *GreetServer) Tool3(ctx context.Context, req *Tool3Request) (*Tool3Response, error) {
 	return &Tool3Response{
 		HisFavoriteFood: "Hello, " + req.WallaceFavoriteFood,
 	}, nil
 }
 
 func main() {
-	err := ServeStdio("protoc-example-mcp", "0.1.0", &GreetServer{})
+	greeter := &GreetServer{}
+
+	err := ServeStdio("protoc-example-mcp", "0.1.0", greeter, greeter)
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"text/template"
 
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
@@ -39,7 +40,14 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) {
 	g := gen.NewGeneratedFile(filename, file.GoImportPath)
 
 	funcMap := template.FuncMap{
-		"toLower": strings.ToLower,
+		"toLower":      strings.ToLower,
+		"fieldType":    getFieldType,
+		"parseFunc":    getParseFunction,
+		"mcpType":      getMcpType,
+		"isRepeated":   isRepeated,
+		"formatOutput": formatOutput,
+		"defaultValue": getDefaultValue,
+		"getBaseType":  getBaseType,
 	}
 
 	tmpl, err := template.New("mcpserver").Funcs(funcMap).Parse(mcpServerTemplate)
@@ -71,10 +79,161 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) {
 	g.P(builder.String())
 }
 
-const mcpServerTemplate = `package {{ .PackageName }}
+// isRepeated checks if a field is a repeated field (array/slice)
+func isRepeated(field *protogen.Field) bool {
+	return field.Desc.Cardinality() == protoreflect.Repeated && !field.Desc.IsMap()
+}
+
+// getFieldType returns the Go type of a protobuf field
+func getFieldType(field *protogen.Field) string {
+	if isRepeated(field) {
+		baseType := getBaseType(field)
+		return "[]" + baseType
+	}
+
+	return getBaseType(field)
+}
+
+// getBaseType returns the base Go type of a protobuf field
+func getBaseType(field *protogen.Field) string {
+	switch field.Desc.Kind() {
+	case protoreflect.BoolKind:
+		return "bool"
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		return "int32"
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return "uint32"
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return "int64"
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return "uint64"
+	case protoreflect.FloatKind:
+		return "float32"
+	case protoreflect.DoubleKind:
+		return "float64"
+	case protoreflect.StringKind:
+		return "string"
+	case protoreflect.BytesKind:
+		return "[]byte"
+	case protoreflect.MessageKind:
+		return "map[string]interface{}"
+	default:
+		return "string" // default to string for unsupported types
+	}
+}
+
+// getMcpType returns the appropriate MCP type for a protobuf field
+func getMcpType(field *protogen.Field) string {
+	// For repeated fields, use WithArray
+	if isRepeated(field) {
+		return "WithArray"
+	}
+
+	// For non-repeated fields
+	switch field.Desc.Kind() {
+	case protoreflect.BoolKind:
+		return "WithBoolean"
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind,
+		protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
+		protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind,
+		protoreflect.Uint64Kind, protoreflect.Fixed64Kind,
+		protoreflect.FloatKind, protoreflect.DoubleKind:
+		return "WithNumber"
+	case protoreflect.StringKind:
+		return "WithString"
+	case protoreflect.MessageKind:
+		return "WithObject"
+	case protoreflect.BytesKind:
+		return "WithString" // Bytes represented as base64 string
+	default:
+		return "WithString" // Default to string for unsupported types
+	}
+}
+
+// getParseFunction returns the appropriate parse function for a protobuf field
+func getParseFunction(field *protogen.Field) string {
+	// For repeated fields, we would need custom handling in the template
+	if isRepeated(field) {
+		// The template will need to handle array parsing specially
+		return "ParseArray"
+	}
+
+	switch field.Desc.Kind() {
+	case protoreflect.BoolKind:
+		return "ParseBoolean"
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		return "ParseInt32"
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return "ParseUInt32"
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return "ParseInt64"
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return "ParseUInt64"
+	case protoreflect.FloatKind:
+		return "ParseFloat32"
+	case protoreflect.DoubleKind:
+		return "ParseFloat64"
+	case protoreflect.StringKind:
+		return "ParseString"
+	case protoreflect.BytesKind:
+		return "ParseString" // Parse as string, convert to []byte later
+	case protoreflect.MessageKind:
+		return "ParseStringMap"
+	default:
+		return "ParseString" // Default to string for unsupported types
+	}
+}
+
+// getDefaultValue returns the default value for a field type
+func getDefaultValue(field *protogen.Field) string {
+	if isRepeated(field) {
+		return "nil"
+	}
+
+	switch field.Desc.Kind() {
+	case protoreflect.BoolKind:
+		return "false"
+	case protoreflect.StringKind:
+		return "\"\""
+	case protoreflect.BytesKind:
+		return "nil"
+	case protoreflect.MessageKind:
+		return "nil"
+	default:
+		// For all numeric types
+		return "0"
+	}
+}
+
+// formatOutput provides the correct formatting for output fields
+func formatOutput(field *protogen.Field) string {
+	switch field.Desc.Kind() {
+	case protoreflect.BoolKind:
+		return "fmt.Sprintf(\"%v\", res.{{ $field.GoName }})"
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind,
+		protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return "fmt.Sprintf(\"%d\", res.{{ $field.GoName }})"
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind,
+		protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return "fmt.Sprintf(\"%d\", res.{{ $field.GoName }})"
+	case protoreflect.FloatKind, protoreflect.DoubleKind:
+		return "fmt.Sprintf(\"%f\", res.{{ $field.GoName }})"
+	case protoreflect.StringKind:
+		return "res.{{ $field.GoName }}"
+	case protoreflect.BytesKind:
+		return "string(res.{{ $field.GoName }})"
+	default:
+		return "fmt.Sprintf(\"%v\", res.{{ $field.GoName }})"
+	}
+}
+
+const mcpServerTemplate = `
+// Code generated by protoc-gen-mcpserver. DO NOT EDIT.
+package {{ .PackageName }}
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
@@ -83,7 +242,7 @@ import (
 {{- range $service := .Services }}
 type {{ $service.GoName }}McpServer interface {
 	{{- range $method := $service.Methods }}
-	{{ $method.GoName }}(ctx context.Context, req {{ $method.Input.GoIdent.GoName }}) (*{{ $method.Output.GoIdent.GoName }}, error)
+	{{ $method.GoName }}(ctx context.Context, req *{{ $method.Input.GoIdent.GoName }}) (*{{ $method.Output.GoIdent.GoName }}, error)
 	{{- end }}
 }
 
@@ -97,39 +256,113 @@ func Register{{ $service.GoName }}McpServer(s *server.MCPServer, srv {{ $service
 				Title: "{{ $method.GoName }}",
 			}),
 			{{- range $field := $method.Input.Fields }}
-			mcp.WithString("{{ $field.GoName }}", mcp.Description("The name of the person to greet")),
+			mcp.{{ mcpType $field }}("{{ $field.GoName }}", mcp.Description("Parameter {{ $field.GoName }}")),
 			{{- end }}
 		),
 		func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-			res, err := srv.{{ $method.GoName }}(ctx, {{ $method.Input.GoIdent.GoName }}{
-				{{- range $field := $method.Input.Fields }}
-				{{ $field.GoName }}: mcp.ParseString(request, "{{ $field.GoName }}", ""),
-				{{- end }}
-			})
+			req := &{{ $method.Input.GoIdent.GoName }}{}
+			
+			{{- range $field := $method.Input.Fields }}
+			{{- if isRepeated $field }}
+			// Handle repeated field {{ $field.GoName }}
+			if arr, ok := request.Params.Arguments["{{ $field.GoName }}"]; ok && arr != nil {
+				if arrValue, ok := arr.([]interface{}); ok {
+					for _, v := range arrValue {
+						{{- if eq (getBaseType $field) "string" }}
+						if strVal, ok := v.(string); ok {
+							req.{{ $field.GoName }} = append(req.{{ $field.GoName }}, strVal)
+						}
+						{{- else if eq (getBaseType $field) "bool" }}
+						if boolVal, ok := v.(bool); ok {
+							req.{{ $field.GoName }} = append(req.{{ $field.GoName }}, boolVal)
+						}
+						{{- else if or (eq (getBaseType $field) "int32") (eq (getBaseType $field) "int64") }}
+						if numVal, ok := v.(float64); ok { // JSON numbers are float64
+							req.{{ $field.GoName }} = append(req.{{ $field.GoName }}, {{ getBaseType $field }}(numVal))
+						}
+						{{- else if or (eq (getBaseType $field) "uint32") (eq (getBaseType $field) "uint64") }}
+						if numVal, ok := v.(float64); ok && numVal >= 0 {
+							req.{{ $field.GoName }} = append(req.{{ $field.GoName }}, {{ getBaseType $field }}(numVal))
+						}
+						{{- else if or (eq (getBaseType $field) "float32") (eq (getBaseType $field) "float64") }}
+						if numVal, ok := v.(float64); ok {
+							req.{{ $field.GoName }} = append(req.{{ $field.GoName }}, {{ getBaseType $field }}(numVal))
+						}
+						{{- else }}
+						// Unsupported array element type
+						{{- end }}
+					}
+				}
+			}
+			{{- else }}
+			req.{{ $field.GoName }} = mcp.{{ parseFunc $field }}(request, "{{ $field.GoName }}", {{ defaultValue $field }})
+			{{- end }}
+			{{- end }}
+			
+			res, err := srv.{{ $method.GoName }}(ctx, req)
 			if err != nil {
 				return nil, err
 			}
+			
 			result := &mcp.CallToolResult{
 				Result:  mcp.Result{},
 				Content: []mcp.Content{},
 				IsError: false,
 			}
+			
 			{{- range $field := $method.Output.Fields }}
-			result.Content = append(result.Content, mcp.NewTextContent("{{ $field.GoName }}: "+res.{{ $field.GoName }}))
+			{{- if isRepeated $field }}
+			// Format repeated field
+			if len(res.{{ $field.GoName }}) > 0 {
+				arrayStr := "["
+				for i, v := range res.{{ $field.GoName }} {
+					if i > 0 {
+						arrayStr += ", "
+					}
+					{{- if eq (getBaseType $field) "string" }}
+					arrayStr += fmt.Sprintf("%q", v)
+					{{- else }}
+					arrayStr += fmt.Sprintf("%v", v)
+					{{- end }}
+				}
+				arrayStr += "]"
+				result.Content = append(result.Content, mcp.NewTextContent("{{ $field.GoName }}: " + arrayStr))
+			} else {
+				result.Content = append(result.Content, mcp.NewTextContent("{{ $field.GoName }}: []"))
+			}
+			{{- else }}
+			// Format non-repeated field
+			{{- if eq (fieldType $field) "string" }}
+			result.Content = append(result.Content, mcp.NewTextContent("{{ $field.GoName }}: " + res.{{ $field.GoName }}))
+			{{- else if eq (fieldType $field) "[]byte" }}
+			result.Content = append(result.Content, mcp.NewTextContent("{{ $field.GoName }}: " + string(res.{{ $field.GoName }})))
+			{{- else }}
+			result.Content = append(result.Content, mcp.NewTextContent("{{ $field.GoName }}: " + fmt.Sprintf("%v", res.{{ $field.GoName }})))
 			{{- end }}
+			{{- end }}
+			{{- end }}
+			
 			return result, nil
 		},
 	)
 	{{- end }}
 }
+{{- end }}
 
-func ServeStdio(name, version string, srv {{ $service.GoName }}McpServer) error {
+func ServeStdio(
+name, 
+version string, 
+{{- range $service := .Services }}
+srv{{ $service.GoName }} {{ $service.GoName }}McpServer,
+{{- end }}
+) error {
 	s := server.NewMCPServer(name, version,
 		server.WithToolCapabilities(true),
 		server.WithLogging(),
 	)
-	Register{{ $service.GoName }}McpServer(s, srv)
+{{- range $service := .Services }}
+	Register{{ $service.GoName }}McpServer(s, srv{{ $service.GoName }})
+{{- end }}
 	return server.ServeStdio(s)
 }
-{{- end }}
 `
